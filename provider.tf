9b507775db5dedfb0939e53648e9e514286bb68c
balaji2353 <balaji2353@gmail.com>
2026-03-13 18:11:44 +0530

    Deploy_a_new_EC2_instance

--- provider.tf
+++ provider.tf
@@ -1,13 +1,13 @@
 terraform {
+          required_version = ">= 1.0" 
           required_providers {
-            aws = { source = "hashicorp/aws" 
-            version = "6.36.0"
-            required_version = ">= 1.0" 
+            aws = { 
+            source = "hashicorp/aws" 
+            version = "6.36.0"           
           } 
           } 
           }
-          provider "aws" { region = "us-west-2" }
+          provider "aws" { 
+           region = "us-west-2" 
+           }
 
-# terraform {
-#   required_version = ">= 1.0" 
-# }
