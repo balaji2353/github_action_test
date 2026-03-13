a24f0bd9e604fe9abfdd14b44e59bd1a1d73951c
balaji2353 <balaji2353@gmail.com>
2026-03-13 18:20:41 +0530

    Deploy_a_new_EC2_instance

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,12 @@
-#tfsec:ignore:aws-ec2-enable-at-rest-encryption
-resource "aws_instance" "webserver" { 
-    ami = "ami-03caad32a158f72db"
-    instance_type = "t2.micro"
-    tags ={
-        Name = "web-server"
-    }
+resource "aws_instance" "webserver" {
+  ami           = "ami-03caad32a158f72db"
+  instance_type = "t2.micro"
+
+  metadata_options {
+    http_tokens = "required"
+  }
+
+  tags = {
+    Name = "web-server"
+  }
 }
