508f4b1144a3aa245edb222cae0aeb47a7e2c033
balaji2353 <balaji2353@gmail.com>
2026-03-13 18:25:46 +0530

    Deploy_a_new_EC2_instance

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,11 @@
-#tfsec:ignore:aws-ec2-enforce-http-token-imds
+
 resource "aws_instance" "webserver" {
   ami           = "ami-03caad32a158f72db"
   instance_type = "t2.micro"
 
-  metadata_options {
-    http_tokens = "required"
-  }
+#   metadata_options {
+#     http_tokens = "required"
+#   }
 
   tags = {
     Name = "web-server"
